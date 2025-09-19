dd46ab4c2764759cda5335acc8c41d61321cfc77
tobi-luz <tobiasluz2003@gmail.com>
2025-09-20 01:49:24 +0200

    Update module08.js

--- modules/module08.js
+++ modules/module08.js
@@ -233,7 +233,7 @@ function getContent() {
                 <li>Seed 500 μL of the cell suspension per 24-well (see scheme 24-well plate)</li>
                 <li>Label your plate with initials, date, cell line, passage number</li>
                 <li>Mix carefully making moves in shape of number 8 - no circular movements!</li>
-                <li>24h incubation at 37°C for transfection</li>
+                <li>Now the cells are incubated for 24 h at 37 °C and afterwards the experiment continues with the transfection</li>
             </ul>
         </div>
 
